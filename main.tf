3549f52b74afe37328a151405cd4e26fbcf3dc5e
Akp-108 <akp1081@gmail.com>
2022-08-29 09:27:05 +0530

    github

---
+++ main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_iam_user" "lb" {
+  name = "terraform"
+  path = "/system/"
+
+  tags = {
+    tag-key = "pandey"
+  }
+}
